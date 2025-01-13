60bcdc5c80706c0e1ad305abc796addeed2ada3d
SociallyIneptWeeb <sociallyinept.csit@gmail.com>
2025-01-13 19:18:39 +0800

    Remove ascii art and non encoding compliant characters

--- src/main/java/SunderRay.java
+++ src/main/java/SunderRay.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,26 +14,10 @@ public class SunderRay {
 
     public static void introduce() {
         String name = "Sunder Ray";
-        try {
-            BufferedReader in = new BufferedReader(new FileReader("src/picture.txt"));
-            String line;
-            while((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
-            in.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Picture file was not found.");
-            return;
-        } catch (IOException e) {
-            System.out.println("Error reading picture file.");
-            return;
-        }
-
-        printDivider();
         System.out.printf("""
                 Ray: H-Hey! It's not like I want to introduce myself or anything, but... my name is
                 %s. (¬`‸´¬) W-What do you want me to do for you, huh? It's not like I care
-                or anything! (💢,,>﹏<,,) b-baka!
+                or anything! (,,>.<,,) b-baka!
                 """, name);
     }
 
@@ -162,7 +142,7 @@ public class SunderRay {
         System.out.println("""
                 Sure, bye or whatever. I-I mean, it's not like I want to see you again or
                 anything... But, well, if you show up, I guess it wouldn’t be that bad.
-                S-See you soon, maybe! ૮₍ ˶>⤙<˶  ₎ა""");
+                S-See you soon, maybe! (｡>\\\\<)""");
     }
 
     public static void main(String[] args) {
